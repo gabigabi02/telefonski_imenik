353cceae5ce632b729bfeca071b3b72737b8c8ca
gabigabi02 <gabriela.dilber@skole.hr>
2024-06-11 19:15:04 +0200

    telefonski imenik

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace TelefonskiImenik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 
+        /// Required designer variable.
+        /// 
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Prazno = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Broj = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Kontakt = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(295, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Telefonski imenik";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(571, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Press";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Prazno,
+            this.Broj,
+            this.Kontakt});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(34, 48);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(300, 360);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged_1);
+            // 
+            // Prazno
+            // 
+            this.Prazno.Text = "#";
+            this.Prazno.Width = 40;
+            // 
+            // Broj
+            // 
+            this.Broj.Text = "Broj";
+            this.Broj.Width = 100;
+            // 
+            // Kontakt
+            // 
+            this.Kontakt.Text = "Kontakt";
+            this.Kontakt.Width = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Prazno;
+        private System.Windows.Forms.ColumnHeader Broj;
+        private System.Windows.Forms.ColumnHeader Kontakt;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelefonskiImenik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+        
+        public void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int[] integers = new int[]
+    {
+        4898076, 6935329, 9338121, 8747950, 3487108, 3412756, 3126020,
+        3351987, 4627060, 4859623, 3960873, 4145597, 2519900, 3374144,
+        3018099, 5211086, 5774242, 4834495, 9793622, 2050801, 6421184,
+        4936783, 6770735, 6216823, 8268708, 6230003, 5613602
+    };
+
+            string[] names = new string[]
+            {
+        "Adrijan", "Bojan", "Boris", "Dalibor", "Dario", "Ivan", "Jadranko",
+        "Janko", "Marko", "Matej", "Mirko", "Mladen", "Nino", "Oliver",
+        "Petar", "Slavko", "Stipe", "Stjepan", "Tomislav", "Toni", "Vedran",
+        "Vladimir", "Zoran", "Zvonko", "Željko", "Jure", "Ivica", "Domagoj"
+            };
+
+            for (int i = 0; i < Math.Min(integers.Length, names.Length); i++)
+            {
+                ListViewItem item = new ListViewItem((i + 1).ToString());
+                item.SubItems.Add(integers[i].ToString());
+                item.SubItems.Add(names[i]);
+                listView1.Items.Add(item);
+            }
+        }
+    }
+}
