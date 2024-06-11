e6f625a3cbabdf29189f46f1d14fa6d65dd03d78
gabigabi02 <gabriela.dilber@skole.hr>
2024-06-11 19:34:16 +0200

    test

--- Program.cs
+++ Program.cs
@@ -9,11 +9,12 @@ namespace TelefonskiImenik
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application  .
-        /// </summary>
+        /// The main entry point for the application .
+        /// </summary >
         [STAThread]
         static void Main()
         {
+            // test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
