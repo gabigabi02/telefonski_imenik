e9afb71bee7086f032b244176e79844975b11445
gabigabi02 <gabriela.dilber@skole.hr>
2024-06-11 19:34:52 +0200

    telefonski imenik

--- Program.cs
+++ Program.cs
@@ -14,8 +14,8 @@ namespace TelefonskiImenik
         [STAThread]
         static void Main()
         {
-            // test
             Application.EnableVisualStyles();
+
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
