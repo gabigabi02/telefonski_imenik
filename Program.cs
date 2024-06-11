1840d05e457203559d556261a39096aced1961a3
gabigabi02 <gabriela.dilber@skole.hr>
2024-06-11 19:22:39 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace TelefonskiImenik
             Application.EnableVisualStyles();
 
             Application.SetCompatibleTextRenderingDefault(false);
+
             Application.Run(new Form1());
         }
     }
