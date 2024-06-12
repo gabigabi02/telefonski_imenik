f25b0d5da6ef5291f18c713d190f5e825f34f6f3
gabigabi02 <gabriela.dilber@skole.hr>
2024-06-12 10:20:47 +0200

    telefonski imenik

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,24 +37,30 @@ namespace TelefonskiImenik
             this.Prazno = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Broj = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Kontakt = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(295, 9);
+            this.label1.Location = new System.Drawing.Point(221, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(203, 29);
-            this.label1.TabIndex = 0;
+            this.label1.TabIndex = 1;
             this.label1.Text = "Telefonski imenik";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(571, 352);
+            this.button1.Location = new System.Drawing.Point(354, 285);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 56);
+            this.button1.Size = new System.Drawing.Size(120, 46);
             this.button1.TabIndex = 2;
             this.button1.Text = "Press";
             this.button1.UseVisualStyleBackColor = true;
@@ -67,9 +73,10 @@ namespace TelefonskiImenik
             this.Broj,
             this.Kontakt});
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(34, 48);
+            this.listView1.Location = new System.Drawing.Point(26, 39);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(300, 360);
+            this.listView1.Size = new System.Drawing.Size(226, 293);
             this.listView1.TabIndex = 3;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -90,14 +97,58 @@ namespace TelefonskiImenik
             this.Kontakt.Text = "Kontakt";
             this.Kontakt.Width = 100;
             // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(354, 258);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(350, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Unesite ime:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(351, 333);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(351, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(241, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ime napišite velikim početnim slovom!";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(604, 361);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -114,6 +165,10 @@ namespace TelefonskiImenik
         private System.Windows.Forms.ColumnHeader Prazno;
         private System.Windows.Forms.ColumnHeader Broj;
         private System.Windows.Forms.ColumnHeader Kontakt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,53 +12,80 @@ namespace TelefonskiImenik
 {
     public partial class Form1 : Form
     {
+        int[] broj = new int[]
+{
+        4898076, 6935329, 9338121, 8747950, 3487108, 3412756, 3126020,
+        3351987, 4627060, 4859623, 3960873, 4145597, 2519900, 3374144,
+        3018099, 5211086, 5774242, 4834495, 9793622, 2050801, 6421184,
+        4936783, 6770735, 6216823, 8268708, 6230003, 5613602
+};
+        string[] ime = new string[]
+        {
+        "Adrijan", "Bojan", "Boris", "Dalibor", "Dario", "Ivan", "Jadranko",
+        "Janko", "Marko", "Matej", "Mirko", "Mladen", "Nino", "Oliver",
+        "Petar", "Slavko", "Stipe", "Stjepan", "Tomislav", "Toni", "Vedran",
+        "Vladimir", "Zoran", "Zvonko", "Željko", "Jure", "Ivica", "Domagoj"
+        };
         public Form1()
         {
             InitializeComponent();
 
-
+            //this.Hide();
+            //Form2 myForm2 = new Form2();
+            //myForm2.StartPosition = FormStartPosition.Manual;
+            //myForm2.Location = new Point(this.Bounds.Right, this.Bounds.Top);
+            //myForm2.Show();
+            
         }
-
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
-
         public void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
         {
 
         }
         
-        public void button1_Click(object sender, EventArgs e)
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-           
+
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        public void Form1_Load(object sender, EventArgs e)
         {
-            int[] integers = new int[]
-    {
-        4898076, 6935329, 9338121, 8747950, 3487108, 3412756, 3126020,
-        3351987, 4627060, 4859623, 3960873, 4145597, 2519900, 3374144,
-        3018099, 5211086, 5774242, 4834495, 9793622, 2050801, 6421184,
-        4936783, 6770735, 6216823, 8268708, 6230003, 5613602
-    };
+         
+      
 
-            string[] names = new string[]
+            for (int i = 0; i < Math.Min(broj.Length, ime.Length); i++)
             {
-        "Adrijan", "Bojan", "Boris", "Dalibor", "Dario", "Ivan", "Jadranko",
-        "Janko", "Marko", "Matej", "Mirko", "Mladen", "Nino", "Oliver",
-        "Petar", "Slavko", "Stipe", "Stjepan", "Tomislav", "Toni", "Vedran",
-        "Vladimir", "Zoran", "Zvonko", "Željko", "Jure", "Ivica", "Domagoj"
-            };
-
-            for (int i = 0; i < Math.Min(integers.Length, names.Length); i++)
-            {
-                ListViewItem item = new ListViewItem((i + 1).ToString());
-                item.SubItems.Add(integers[i].ToString());
-                item.SubItems.Add(names[i]);
-                listView1.Items.Add(item);
+                ListViewItem kontakt = new ListViewItem((i + 1).ToString());
+                kontakt.SubItems.Add(broj[i].ToString());
+                kontakt.SubItems.Add(ime[i]);
+                listView1.Items.Add(kontakt);
             }
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            int index = -1;
+            string unos = textBox1.Text;
+            index = Array.IndexOf(ime, unos);
+
+            if (index >= 0)
+            {
+                int broj1 = broj[index];
+                label3.Text = $"Broj korisnika '{unos}': {broj1}";
+            }
+            else
+            {
+                label3.Text = $"Korisnik '{unos}' ne postoji!";
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
